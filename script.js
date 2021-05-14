8249e098efbe4198c340871c81a11310747cd070
Georgy Y <y-georgy@yandex.ru>
2021-05-14 13:54:59 +0300

    my fixes

--- script.js
+++ script.js
@@ -2,31 +2,28 @@ let profile = document.querySelector('.profile');
 
 function openPopup () {
   let popup = profile.querySelector('.popup');
-  let popupWindow = popup.querySelector('.popup__window');
   let profileTitle = profile.querySelector('.profile__title');
   let profileSubtitle = profile.querySelector('.profile__sublitle');
-  let popupInputTitle = profile.querySelector('.popup__input-title');
-  let popupInputSubtitle = profile.querySelector('.popup__input-subtitle');
-  popup.classList.remove('popup_disabled');
-  popupWindow.classList.remove('popup__window_disabled');
-  popupInputTitle.setAttribute('value', profileTitle.textContent);
-  popupInputSubtitle.setAttribute('value', profileSubtitle.textContent);
+  let nameInput = profile.querySelector('.popup__input-name');
+  let jobInput = profile.querySelector('.popup__input-job');
+  popup.classList.add('popup_opened');
+  nameInput.setAttribute('value', profileTitle.textContent);
+  jobInput.setAttribute('value', profileSubtitle.textContent);
 }
 
 function closePopup () {
   let popup = profile.querySelector('.popup');
-  let popupWindow = popup.querySelector('.popup__window');
-  popup.classList.add('popup_disabled');
-  popupWindow.classList.add('popup__window_disabled');
+  popup.classList.remove('popup_opened');
 }
 
-function savePopupInfo () {
-  let popupInputTitle = profile.querySelector('.popup__input-title');
-  let popupInputSubtitle = profile.querySelector('.popup__input-subtitle');
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  let nameInput = profile.querySelector('.popup__input-name');
+  let jobInput = profile.querySelector('.popup__input-job');
   let profileTitle = profile.querySelector('.profile__title');
   let profileSubtitle = profile.querySelector('.profile__sublitle');
-  profileTitle.textContent = popupInputTitle.value;
-  profileSubtitle.textContent = popupInputSubtitle.value;
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
   closePopup ();
 }
 
@@ -36,5 +33,5 @@ editButton.addEventListener('click', openPopup);
 let closeButton = profile.querySelector('.popup__icon-close');
 closeButton.addEventListener('click', closePopup);
 
-let popupButtonSave = profile.querySelector('.popup__button-save');
-popupButtonSave.addEventListener('click', savePopupInfo);
+let formElement = profile.querySelector('.popup__submit-button');
+formElement.addEventListener('click', formSubmitHandler);
