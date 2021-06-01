4cb767d4fc4e10f698682c8044b614737f8d1036
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-06-01 10:11:47 +0300

    refactor: script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,59 @@
-let profile = document.querySelector('.profile');
-let profileTitle = profile.querySelector('.profile__title');
-let profileSubtitle = profile.querySelector('.profile__sublitle');
-let popup = document.querySelector('.popup');
-let nameInput = popup.querySelector('.popup__input-name');
-let jobInput = popup.querySelector('.popup__input-job');
-let editButton = profile.querySelector('.profile__edit-button');
-let closeButton = popup.querySelector('.popup__icon-close');
-let formElement = popup.querySelector('.popup__container');
+// профиль автора
+const profile = document.querySelector('.profile');
+const profileTitle = profile.querySelector('.profile__title');
+const profileSubtitle = profile.querySelector('.profile__sublitle');
 
+// попап для ввода данных автора
+const popup = document.querySelector('.popup');
+const nameInput = popup.querySelector('.popup__input-name');
+const jobInput = popup.querySelector('.popup__input-job');
+
+// кнопки
+const editButton = profile.querySelector('.profile__edit-button');
+const closeButton = popup.querySelector('.popup__icon-close');
+const formElement = popup.querySelector('.popup__container');
+
+// массив с данными карточек
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// открытие попапа
 function openPopup () {
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
   popup.classList.add('popup_opened');
 }
 
+// закрытие попапа
 function closePopup () {
   popup.classList.remove('popup_opened');
 }
 
+// сохранение
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
@@ -25,6 +61,7 @@ function formSubmitHandler (evt) {
   closePopup();
 }
 
+// слушатели
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
